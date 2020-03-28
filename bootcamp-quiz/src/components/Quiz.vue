ea3ac96159fba156fb37ec7bfc80f3d7ba67e1f9
Tony Kark <tonywebdevkark@gmail.com>
2020-03-28 20:59:20 +0300

    bootcamp-quiz list added

--- bootcamp-quiz/src/components/Quiz.vue
+++ bootcamp-quiz/src/components/Quiz.vue
@@ -2,7 +2,14 @@
   <b-jumbotron>
     <template v-slot:lead>{{question.question}}</template>
     <hr class="my-4" />
-    <p>Answers</p>
+    <b-list-group>
+      <b-list-group-item
+        @click.prevent="indexSelected(index)"
+        v-for="(answer, index) in answers"
+        :key="index"
+        :class="[selectedIndex === index ? 'selected' : '']"
+      >{{ answer }}</b-list-group-item>
+    </b-list-group>
     <b-button variant="primary" href="#">Send Answer</b-button>
     <b-button @click="next" variant="success" href="#">Next Question</b-button>
   </b-jumbotron>
@@ -13,6 +20,35 @@ export default {
   props: {
     question: Object,
     next: Function
+  },
+  data() {
+    return {
+      selectedIndex: null
+    };
+  },
+  computed: {
+    answers() {
+      let answers = [...this.question.incorrect_answers];
+      answers.push(this.question.correct_answer);
+      return answers;
+    }
+  },
+  methods: {
+    indexSelected(index) {
+      this.selectedIndex = index;
+    }
   }
 };
 </script>
+
+<style scoped>
+.correct {
+  background-color: green;
+}
+.incorrect {
+  background-color: red;
+}
+.selected {
+  background-color: blue;
+}
+</style>
